5197cc9cebf6a1421a9ab56e708abba42355baef
porushyadav <37874551+porushyadav@users.noreply.github.com>
2020-09-26 16:14:11 +0530

    Updates

---
+++ precache-manifest.efced4da45b3932d130c6997440a098c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa5374286eb02306e3a0",
+    "url": "/UIDesign-Kredence/static/css/main.9d76df9f.chunk.css"
+  },
+  {
+    "revision": "aa5374286eb02306e3a0",
+    "url": "/UIDesign-Kredence/static/js/main.e726938a.chunk.js"
+  },
+  {
+    "revision": "c9d418d483457f0aa99f",
+    "url": "/UIDesign-Kredence/static/js/runtime~main.76278d46.js"
+  },
+  {
+    "revision": "e298f1f8f0fa064d31fd",
+    "url": "/UIDesign-Kredence/static/js/2.5ef774bb.chunk.js"
+  },
+  {
+    "revision": "e183f7661584f97f79779fe6b91c295d",
+    "url": "/UIDesign-Kredence/index.html"
+  }
+];
